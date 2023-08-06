1f0a34abdbd83c054ec2a9cf451806311d138f80
Mjhenning <126242379+Mjhenning@users.noreply.github.com>
2023-08-06 10:30:18 +0200

    Basis of Pickup System is in-place

---
+++ GX300-3 Open World Game/Assets/_Project/Scripts/Items/Item.cs
@@ -0,0 +1,7 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour { //script used to attatch the item type to a gameobject
+    public ItemObject item;
+}
